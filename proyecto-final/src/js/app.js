cc1154aa84073b5a579b14ca94f0cacb9954b776
jaimeviloriogreen <jaimeviloriogreen@hotmail.com>
2023-07-31 21:42:26 -0400

    movies added

--- proyecto-final/src/js/app.js
+++ proyecto-final/src/js/app.js
@@ -1,6 +1,12 @@
 const header = document.querySelector(".header");
 
 window.addEventListener("scroll", ()=>{
-    if(window.scrollY >= 40) header.classList.add("header--active");
-    if(window.scrollY <= 20) header.classList.remove("header--active");
+    if(window.scrollY >= 40) {
+        header.classList.add("header--active");
+        header.classList.add("header--boxshadow");
+    };
+    if(window.scrollY <= 20){
+        header.classList.remove("header--boxshadow");
+        header.classList.remove("header--active");
+    };
 });
